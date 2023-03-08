355bf1654cb90f356d1c1c3ae85fbaec839b68de
serapErkan <serapperkan@gmail.com>
2023-03-08 10:08:32 +0300

    product sales transactions

--- src/components/products/ProductSell.vue
+++ src/components/products/ProductSell.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+  <div class="container">
     <!-- <div class="loading" >
       <div class="lds-ripple">
         <div></div>
@@ -10,28 +10,42 @@
       <div class="col-6 offset-3 pt-3 card mt-5 shadow">
         <div class="card-body">
           <h3>Ürün Çıkışı</h3>
-          <hr>
+          <hr />
           <div class="form-group">
             <label>Ürün Adı</label>
-            <select class="form-control" v-model="selectedProduct" @change="productChange">
+            <select
+              class="form-control"
+              v-model="selectedProduct"
+              @change="productChange"
+            >
               <option selected disabled>Lütfen Bir Ürün Seçiniz...</option>
-              <option 
-            :disabled="product.count == 0"
-              :value="product.key"
-              v-for="product in products">{{ product.title }}</option>
-             
+              <option
+                :disabled="product.count == 0"
+                :value="product.key"
+                v-for="product in products"
+                >{{ product.title }}</option
+              >
             </select>
           </div>
           <transition name="fade" mode="out-in">
-            <div class="card mb-4 mt-4 border border-danger" v-if="product !== null" >
+            <div
+              class="card mb-4 mt-4 border border-danger"
+              v-if="product !== null"
+            >
               <div class="card-body">
                 <div class="row">
                   <div class="col-12 text-center">
                     <div class="mb-3">
-                      <span class="badge text-bg-warning fs-4">Stok :{{product.count}}</span>
-                      <span class="badge text-bg-warning fs-4">Fiyat :{{product.price | currency }} </span>
+                      <span class="badge text-bg-warning fs-4"
+                        >Stok :{{ product.count }}</span
+                      >
+                      <span class="badge text-bg-warning fs-4"
+                        >Fiyat :{{ product.price | currency }}
+                      </span>
                     </div>
-                    <p class="border border-warning p-2 text-secondary"> {{ product.description }} </p>
+                    <p class="border border-warning p-2 text-secondary">
+                      {{ product.description }}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -39,46 +53,52 @@
           </transition>
           <div class="form-group">
             <label>Adet</label>
-            <input type="text" class="form-control" placeholder="Ürün adetini giriniz..">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Ürün adetini giriniz.."
+              v-model="selectedCount"
+            />
           </div>
-          <hr>
-          <button  class="btn btn-primary">Kaydet</button>
+          <hr />
+          <button class="btn btn-primary" @click="save">Kaydet</button>
         </div>
       </div>
     </div>
   </div>
-    
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
-export default{
-data(){
-    return{
-        selectedProduct:null,
-        product:null,
+export default {
+  data() {
+    return {
+      selectedProduct: null,
+      product: null,
+      selectedCount: null
+    };
+  },
+  computed: {
+    ...mapGetters({
+      products: "getPoducts"
+    })
+  },
+
+  methods: {
+    productChange() {
+      //  console.log(this.selectedProduct)
+      // ---NPY-aRatMf4hJbDbjg2
+      this.product = this.$store.getters.getPoduct(this.selectedProduct)[0];
+      // console.log(this.product);
+      // console.log(this.product.price);
+    },
+    save() {
+      let product = {
+        key: this.selectedProduct, // key
+        count: this.selectedCount // adet-count
+      };
+      this.$store.dispatch("sellProduct", product);
     }
-},
-computed:{
- ...mapGetters({
-    products:'getPoducts'
- })
-    
- 
-},
-
-methods:{
-    productChange(){
-
-       console.log(this.selectedProduct)
-       // ---NPY-aRatMf4hJbDbjg2
-       this.product = this.$store.getters.getPoduct(this.selectedProduct)[0];
-       console.log(this.product);
-       console.log(this.product.price);
-    }
-}
-
-
-
-}
+  }
+};
 </script>
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,6 +1,5 @@
-
 import Vue from "vue";
-import {router } from "../../router";
+import { router } from "../../router";
 
 const state = {
   products: []
@@ -12,79 +11,106 @@ const getters = {
     return state.products;
   },
   getPoduct(state) {
-//ürünün detaylarını getiricek   
-//  filter ile gelene key değerine eşitse detay getir 
-// özel bir durum 
-return key =>state.products.filter(el=>{
-return el.key==key;
-  
-})
-
+    //ürünün detaylarını getiricek
+    //  filter ile gelene key değerine eşitse detay getir
+    // özel bir durum
+    return key =>
+      state.products.filter(el => {
+        return el.key == key;
+      });
   }
-
-
 };
 const mutations = {
-    //state güncellemek için -senkron
-updateProductList(state,product){
-  state.products.push(product)
-}  
-
+  //state güncellemek için -senkron
+  updateProductList(state, product) {
+    state.products.push(product);
+  }
 };
 const actions = {
-// asenkron 
-initApp({commit}){
-    // vue Resource işlemleri 
-Vue.http.get("https://productoperations-deece-default-rtdb.firebaseio.com/products.json")
-.then((res)=>{
- let data=res.body;
+  // asenkron
+  initApp({ commit }) {
+    // vue Resource işlemleri
+    Vue.http
+      .get(
+        "https://productoperations-deece-default-rtdb.firebaseio.com/products.json"
+      )
+      .then(res => {
+        let data = res.body;
 
-//  console.log("res-body",data);
-for (let key in data){
+        //  console.log("res-body",data);
+        for (let key in data) {
+          data[key].key = key;
+          // console.log("key",data[key]);
 
-  data[key].key=key;
-  // console.log("key",data[key]);
-
-  commit("updateProductList",data[key]);
-}
-
-})
-},
-saveProduct({dispatch,commit,state},payload){
-  // vue Resource işlemleri 
-  // this.$http -- vue import
-  // http ile geçiçi tanımlayalım 
- 
-    Vue.http.post("https://productoperations-deece-default-rtdb.firebaseio.com/products.json",payload)
-    .then((response)=>{
-    
-  /* ürünleri güncellemek için*/
-  
-  payload.key=response.body.name;
-  commit('updateProductList',payload);
-  console.log(response,state);
-
-//  alıştış bakiye bilgilerini güncelleyelim
-let tradeResult={
-
-  purchase:payload.price,
-  count:payload.count,
-  sale:0.0
-
-}
-dispatch("setTradeResult",tradeResult);
-router.replace("/");
-
-    })
-  
-  
-},
-sellProduct({comit},payload){
-    // vue Resource işlemleri 
+          commit("updateProductList", data[key]);
+        }
+      });
   },
+  saveProduct({ dispatch, commit, state }, payload) {
+    // vue Resource işlemleri
+    // this.$http -- vue import
+    // http ile geçiçi tanımlayalım
 
+    Vue.http
+      .post(
+        "https://productoperations-deece-default-rtdb.firebaseio.com/products.json",
+        payload
+      )
+      .then(response => {
+        /* ürünleri güncellemek için*/
 
+        payload.key = response.body.name;
+        commit("updateProductList", payload);
+        // console.log(response, state);
 
+        //  alıştış bakiye bilgilerini güncelleyelim
+        let tradeResult = {
+          purchase: payload.price,
+          count: payload.count,
+          sale: 0.0
+        };
+        dispatch("setTradeResult", tradeResult);
+        router.replace("/");
+      });
+  },
+  sellProduct({ comit, state,dispatch }, payload) {
+    // patch -arry pass by reference- pass by value..
+
+    let product = state.products.filter(el => {
+      return el.key == payload.key;
+    });
+    // console.log("count işlemleri", product);
+
+    if (product) {
+      let totalCount = product[0].count - payload.count;
+      //Updating Data
+      // console.log(totalCount,product[0].count,payload.count);
+      Vue.http
+        .patch(
+          "https://productoperations-deece-default-rtdb.firebaseio.com/products/" +
+            payload.key +
+            ".json",
+          { count: totalCount }
+        )
+        .then(res => {
+          // console.log(res);
+          // verileri anlık güncellemek için
+          product[0].count = totalCount;
+
+          //satış bakiye bilgisi güncelleme 
+          let tradeResult = {
+            purchase: 0,
+            count: payload.count,
+            sale: product[0].price,
+          };
+          dispatch("setTradeResult", tradeResult);
+          router.replace("/");
+
+        });
+    }
+
+    //
+  }
 };
 
 export default {
