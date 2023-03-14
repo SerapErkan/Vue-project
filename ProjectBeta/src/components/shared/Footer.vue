aa08194ed050a6fb3fe55e6879241c8f3f2c0d2e
serapErkan <serapperkan@gmail.com>
2023-03-14 15:56:23 +0300

    Footer section is done

---
+++ ProjectBeta/src/components/shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+<footer class="bg-light text-muted mt-5 py-3 fixed-bottom">
+    <div class="container text-center ">
+      <span>© 2023 Company, Inc. All Rights Reserved.</span>
+      <span class="mx-2">|</span>
+      <a href="mailto:info@company.com" class="text-muted" >İletişim</a>
+    </div>
+  </footer>
+
+    </div>
+   
+</template>
+
